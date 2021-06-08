d6aef2a4e204030d63b1ecf352e2b0aa41e19bd1
abiramipalanivel2001 <abiramipalanivel2001@gmail.com>
2021-06-08 07:57:47 +0700

    init

--- lib/main.dart
+++ lib/main.dart
@@ -6,20 +6,14 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-// This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
-      // Title of App
-      title: 'GFG slider',
+      title: 'home',
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      darkTheme: ThemeData.dark(),
-
-      //First Screen of Slider App
       home: HomePage(),
     );
   }
